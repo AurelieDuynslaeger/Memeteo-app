64a9715b80ba1217024a6b52031af12399aede21
Julie Vandenberghe <julie.vandenberghe.59@gmail.com>
2024-02-20 12:27:07 +0100

    ✨ add and remove a city from favs (in progress), saved in localstorage

--- src/components/SuggestionBox.js
+++ src/components/SuggestionBox.js
@@ -1,12 +1,41 @@
+import React, { useState } from "react";
+import { FaRegStar, FaStar } from "react-icons/fa";
+
 export default function SuggestionBox({ showSuggestions, suggestions, handleSuggestionClick, error }) {
 
   // Etape 1
   // const fav  = get fav from localstorage
+  const [favs, setFavs] = useState(JSON.parse(localStorage.getItem("memeteo-favourites-cities")) || []);
+  const [isFav, setIsFav] = useState(false);
 
-  // isFav() => { return city not in fav }
+  //add a fav into localstorage
+    const addToFavs = (item) => {
+        const newFavouriteList = [...favs, item];
+        setFavs(newFavouriteList);
+        localStorage.setItem("memeteo-favourites-cities", JSON.stringify([...favs, item]));
+        //setIsFav(true);
+  } 
+
+ /*  const addToFavs = (item) => {
+    setFavs(prevFavs => {
+      const updatedFavs = [...prevFavs, item];
+      localStorage.setItem("memeteo-favourites-cities", JSON.stringify(updatedFavs));
+      return updatedFavs;
+    });
+  } */
+
+  //remove a fav from localstorage
+  const removeFromFavs = (index) => {
+    const copyFavs = [...favs]; // Copie du tableau
+    copyFavs.splice(index, 1); // On supprime un élément du tableau copyFav à l'indice spécifié par index
+    setFavs(copyFavs); // On met à jour l'état products du composant avec la nouvelle valeur copyProducts, déclenchant ainsi une ré-renderisation du composant avec le panier mis à jour.
+    localStorage.setItem("memeteo-favourites-cities", JSON.stringify(copyFavs)); // On met à jour la valeur stockée dans le localStorage
+  }
+
+  // isFav() => { return city in fav }
 
   // handleFav () => { 
-  //  if (isFav()) {
+  //  if (!isFav()) {
   //   add to fav > parcourir le tableau des fav et vérifier que l'id ne soit pas dedans
   //  } else {
   //   remove city from fav
@@ -31,6 +60,8 @@ export default function SuggestionBox({ showSuggestions, suggestions, handleSugg
               onClick={() => handleSuggestionClick(item)}
               className="suggestion"> 
                 {item} {/* Here = location name / place name */}
+                <span className="fav" onClick={() => addToFavs(item)}><FaRegStar /></span>
+                <span className="fav" onClick={() => removeFromFavs(index)}><FaStar /></span>
                 { /* Add a span here for fav + class for fav (isFav) */} 
             </li>
           ))}
